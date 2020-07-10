842cc7ba2514d3d2122e168d864fc23899bce2a5
FaiFai <faifai@FaiFai-MacBook-Pro-2018.local>
2020-07-10 12:02:01 +0800

    <fix>(perf):repair the Right Click Search

--- src/background/contextMenus.ts
+++ src/background/contextMenus.ts
@@ -15,89 +15,91 @@ chrome.contextMenus.create({
 (async () => {
     const config = await coreConfig.get()
 
-    if (!config.rightClickSearch) { return }
+    if (config.rightClickSearch) { 
+        const searchEngines = [
+            {
+                name: 'Cloudopt',
+                url: 'https://s.cloudopt.net/search?q={{word}}'
+            },
+            {
+                name: 'Google',
+                url: 'https://www.google.com.hk/#&q={{word}}&ie=utf-8'
+            },
+            {
+                name: 'Baidu',
+                url: 'https://www.baidu.com/s?wd={{word}}'
+            }
+        ]
 
-    const searchEngines = [
-        {
-            name: 'Cloudopt',
-            url: 'https://s.cloudopt.net/search?q={{word}}'
-        },
-        {
-            name: 'Google',
-            url: 'https://www.google.com.hk/#&q={{word}}&ie=utf-8'
-        },
-        {
-            name: 'Baidu',
-            url: 'https://www.baidu.com/s?wd={{word}}'
+        for (let searchEngine of searchEngines) {
+            chrome.contextMenus.create( {
+                title: `Search by ${searchEngine.name}`,
+                parentId: 'cloudopt',
+                contexts: ['selection'],
+                onclick: async (info) => {
+                    window.open(searchEngine.url.replace('{{word}}',info.selectionText))
+                },
+            })
         }
-    ]
 
-    for (let searchEngine of searchEngines) {
         chrome.contextMenus.create( {
-            title: `Search by ${searchEngine.name}`,
+            title: '————————————',
             parentId: 'cloudopt',
             contexts: ['selection'],
-            onclick: async (info) => {
-                window.open(searchEngine.url.replace('{{word}}',info.selectionText))
-            },
+            enabled: false,
         })
     }
 
-    chrome.contextMenus.create( {
-        title: '————————————',
+    chrome.contextMenus.create({
+        title: i18n.get('contextMenus1'),
         parentId: 'cloudopt',
-        contexts: ['selection'],
-        enabled: false,
+        contexts: ['all'],
+        onclick: async (info, tab) => {
+            const result = await coreConfig.fastAddAllowList(tab.url)
+            if (result === coreConfig.AddListResult.SUCCESS) {
+                notification.success(i18n.get('optionTipsAddAllowListSuccess'))
+            } else {
+                coreConfig.fireAddListErrorNotification(result)
+            }
+        },
+    })
+    
+    chrome.contextMenus.create({
+        title: i18n.get('contextMenus4'),
+        parentId: 'cloudopt',
+        contexts: ['all'],
+        onclick: async (info, tab) => {
+            const result = await coreConfig.fastAddBlockList(tab.url)
+            if (result === coreConfig.AddListResult.SUCCESS) {
+                notification.success(i18n.get('optionTipsAddBlockListSuccess'))
+            } else {
+                coreConfig.fireAddListErrorNotification(result)
+            }
+        },
+    })
+    
+    chrome.contextMenus.create({
+        title: i18n.get('contextMenus2'),
+        parentId: 'cloudopt',
+        contexts: ['all'],
+        onclick: (info, tab) => {
+            if (tab.url.startsWith('http://') || tab.url.startsWith('https://')) {
+                tabOp.open(`https://www.cloudopt.net/report/${utils.getHost(tab.url)}`)
+            }
+        },
+    })
+    
+    chrome.contextMenus.create({
+        title: i18n.get('contextMenus3'),
+        parentId: 'cloudopt',
+        contexts: ['all'],
+        onclick: (info, tab) => {
+            logger.debug(`Opening Assistant UI for tab id=${tab.id} url= ${tab.url}`)
+            const backgroundPage = chrome.extension.getBackgroundPage()
+            const adguardApi = backgroundPage.adguardApi
+            adguardApi.openAssistant(tab.id)
+        },
     })
 })()
 
-chrome.contextMenus.create({
-    title: i18n.get('contextMenus1'),
-    parentId: 'cloudopt',
-    contexts: ['all'],
-    onclick: async (info, tab) => {
-        const result = await coreConfig.fastAddAllowList(tab.url)
-        if (result === coreConfig.AddListResult.SUCCESS) {
-            notification.success(i18n.get('optionTipsAddAllowListSuccess'))
-        } else {
-            coreConfig.fireAddListErrorNotification(result)
-        }
-    },
-})
 
-chrome.contextMenus.create({
-    title: i18n.get('contextMenus4'),
-    parentId: 'cloudopt',
-    contexts: ['all'],
-    onclick: async (info, tab) => {
-        const result = await coreConfig.fastAddBlockList(tab.url)
-        if (result === coreConfig.AddListResult.SUCCESS) {
-            notification.success(i18n.get('optionTipsAddBlockListSuccess'))
-        } else {
-            coreConfig.fireAddListErrorNotification(result)
-        }
-    },
-})
-
-chrome.contextMenus.create({
-    title: i18n.get('contextMenus2'),
-    parentId: 'cloudopt',
-    contexts: ['all'],
-    onclick: (info, tab) => {
-        if (tab.url.startsWith('http://') || tab.url.startsWith('https://')) {
-            tabOp.open(`https://www.cloudopt.net/report/${utils.getHost(tab.url)}`)
-        }
-    },
-})
-
-chrome.contextMenus.create({
-    title: i18n.get('contextMenus3'),
-    parentId: 'cloudopt',
-    contexts: ['all'],
-    onclick: (info, tab) => {
-        logger.debug(`Opening Assistant UI for tab id=${tab.id} url= ${tab.url}`)
-        const backgroundPage = chrome.extension.getBackgroundPage()
-        const adguardApi = backgroundPage.adguardApi
-        adguardApi.openAssistant(tab.id)
-    },
-})
